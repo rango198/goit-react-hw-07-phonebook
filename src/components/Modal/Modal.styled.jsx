3dd7653b99980bc019bd1dc0c54deed3fec9acbb
rango198 <137562811+rango198@users.noreply.github.com>
2024-02-09 08:49:20 +0200

    color

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -14,7 +14,7 @@ export const ModalContact = styled.div`
   align-items: center;
   overflow: hidden;
   padding: 5px;
-  background-color: rgba(90, 107, 110, 0.966);
+  background-color: rgba(228, 169, 201, 0.979);
   box-shadow: 0 2px 39px rgba(0, 0, 0, 1.2);
 `;
 
