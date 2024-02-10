01a833a8832a3d8cdc8720743022537b74f55002
rango198 <137562811+rango198@users.noreply.github.com>
2024-02-10 17:38:37 +0200

    fixedWork

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,14 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { phoneBookSlice } from './contacts-slice';
 import { filterSlice } from './filter-slice';
-import { tasksReducer } from './tasksSlice';
-import { filtersReducer } from './filtersSlice';
 
 export const store = configureStore({
   reducer: {
     phoneBook: phoneBookSlice.reducer,
     filter: filterSlice.reducer,
-    tasks: tasksReducer,
-    filters: filtersReducer,
   },
 });
--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'components/redux/store';
-import { AppTask } from 'AppTask';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-      <AppTask />
     </Provider>
   </React.StrictMode>
 );
